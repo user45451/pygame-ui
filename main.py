38c9795cb0e8c53a21f5a7fa5355aa03f9c92a0b
user45451 <lemonleavesremain@gmail.com>
2025-03-17 14:20:44 +0800

    main.py
    编写新的生成嵌套展示效果

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GRAY = (200, 200, 200)
 
+# 定义按钮点击事件函数
+def button1_action():
+    print('按钮1被点击')
+
+def button2_action():
+    print('按钮2被点击')
+
+
 # 主循环
 running = True
 root_ui = ui(0, 0, width, height)
@@ -21,20 +29,18 @@ root_ui = ui(0, 0, width, height)
 
 
 # 修改主循环以使用窗口类
-# 生成2个根节点窗口
-for i in range(2):
-    outer_window = Window(100 + i * 400, 100, 300, 200)
-    # 嵌套生成4层窗口
-    parent_window = outer_window
-    for level in range(3):
-        new_children = []
-        for j in range(3):
-            for k in range(3):
-                inner_window = Window(100 + j * 50, 100 + k * 50, 50, 50)
-                parent_window.add_child(inner_window)
-                new_children.append(inner_window)
-        parent_window = new_children[0] if new_children else parent_window
-    root_ui.add_child(outer_window)
+outer_window = Window(100, 100, 300, 200)
+# 每个窗口内部生成窗口阵列
+def generate(root_window, level):
+    for j in range(3):
+        for k in range(3):
+            inner_window = Window(100 + j * 50, 100 + k * 50, 50, 50)
+            if level - 1 > 0:
+                generate(inner_window, level - 1)
+            root_window.add_child(inner_window)
+root_ui.add_child(outer_window)
+
+generate(outer_window, 2)
 
 
 while running:
