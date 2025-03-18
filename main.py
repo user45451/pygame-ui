14cf751531c2f9fab0973f726ce9a0f69bd40e1f
user45451 <lemonleavesremain@gmail.com>
2025-03-18 22:21:51 +0800

    Add files via upload
    窗口的更新
    文本框的更新

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+from ui import window
 from ui.window import *
+from ui.Text import TextBox
 
 # 初始化pygame
 pygame.init()
@@ -14,22 +16,27 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GRAY = (200, 200, 200)
 
-# 定义按钮点击事件函数
-def button1_action():
-    print('按钮1被点击')
-
-def button2_action():
-    print('按钮2被点击')
-
 
 # 主循环
 running = True
 root_ui = ui(0, 0, width, height)
-
+text_box = TextBox(0, 0, 100, 50, '你好!')
+text_box1 = TextBox(0, 0, 100, 50, '你好!')
 
 
 # 修改主循环以使用窗口类
-outer_window = Window(100, 100, 300, 200)
+window = Window(100, 100, 300, 200)
+window.add_child(text_box)
+
+window1 = Window(100, 300, 300, 200)
+window1.add_child(text_box1)
+
+root_ui.add_child(window)
+root_ui.add_child(window1)
+
+
+# 修改主循环以使用窗口类
+outer_window = Window(400, 100, 300, 200)
 # 每个窗口内部生成窗口阵列
 def generate(root_window, level):
     for j in range(3):
@@ -41,7 +48,6 @@ def generate(root_window, level):
 root_ui.add_child(outer_window)
 
 generate(outer_window, 2)
-
 
 while running:
     for event in pygame.event.get():
--- ui/window.py
+++ ui/window.py
@@ -1,51 +1,28 @@
 from ui.ui import *
-
-class Pointer:
-    value : float
-    def __init__(self, value):
-        self.value = value
-
-class Reflection(ui):
-    def __init__(self, x : Pointer, y : Pointer, width, height):
-        super().__init__(x.value - width / 2, y.value - height / 2, width, height)
-        self.x = x
-        self.y = y
-    def own_draw(self, screen):
-        super().own_draw(screen)
-        self.rect.x = int(self.x.value - self.rect.width / 2)
-        self.rect.y = int(self.y.value - self.rect.height / 2)
-    def hovered_event(self, event):
-        return
-    def drag_event(self, event):
-        # 窗口移动的事件
-        if event:
-            if mouse_state['left']:
-                # 更新窗口位置
-                self.rect.x = event.pos[0] - self.drag_offset_x
-                self.rect.y = event.pos[1] - self.drag_offset_y
-                self.x.value = self.rect.x + self.rect.width / 2
-                self.y.value = self.rect.y + self.rect.height / 2
-    def handle_event(self, event):
-        return False
+from ui.Reflection import *
 
 class Window(ui):
     def __init__(self, x, y, width, height):
         super().__init__(x, y, width, height)
         self.offset_x = 0
         self.offset_y = 0
-        self.color_option = [(100,100,100),(0,255,0),(0,0,255)]
-        self.rect_color = self.color_option[0]
+        self.color = [(100,100,100),(0,255,0),(0,0,255)]
+        self.color_index = 0
 
         self.left = Pointer(x)
         self.top = Pointer(y)
-        self.topleft = Reflection(self.left, self.top, 10, 10)
-
         self.right = Pointer(x + width)
         self.bottom = Pointer(y + height)
-        self.rightbottom = Reflection(self.right, self.bottom, 10, 10)
+
+        self.topleftResize = Reflection(self.left, self.top, 10, 10)
+        self.rightbottomResize = Reflection(self.right, self.bottom, 10, 10)
+        self.toprightResize = Reflection(self.right, self.top, 10, 10)
+        self.leftbottomResize = Reflection(self.left, self.bottom, 10, 10)
     def rebuild(self):
-        self.root_ui.add_child(self.topleft)
-        self.root_ui.add_child(self.rightbottom)
+        self.root_ui.add_child(self.topleftResize)
+        self.root_ui.add_child(self.rightbottomResize)
+        self.root_ui.add_child(self.toprightResize)
+        self.root_ui.add_child(self.leftbottomResize)
     def full(self): # 全局窗口变换, 鼠标移动窗口的时候可能会有比例缩放问题
         bouding_box = [float('inf'), float('inf'), float('-inf'), float('-inf')]
         for child in self.children:
@@ -60,22 +37,27 @@ class Window(ui):
         scaled = min([self.rect.width / box_size[0], self.rect.height / box_size[1]])
         if box_size[0] > 0 and box_size[1] > 0:
             self.inner_gravity = [rect_center[0] - box_center[0], rect_center[1] - box_center[1], box_center[0], box_center[1], self.gravity[4] * scaled, self.gravity[5] * scaled]
+    def move(self): # 相对窗口坐标系变换
+        self.inner_gravity = [self.gravity[0] + self.rect.x, self.gravity[1] + self.rect.y, 0, 0, 1, 1]
     def own_draw(self, screen):
         rect = self.global_rect()
-        pygame.draw.rect(screen, self.rect_color, rect, 2)
+        pygame.draw.rect(screen, self.color[self.color_index], rect, 2)
         # 将浮点数转换为整数以避免类型错误
         self.rect.x = int(self.left.value)
         self.rect.y = int(self.top.value)
         self.rect.width = int(self.right.value - self.left.value)
         self.rect.height = int(self.bottom.value - self.top.value)
         self.full()
+        # self.move()
     def hovered_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                self.rect_color = self.color_option[1]
+                self.color_index = 1
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
-                self.rect_color = self.color_option[0]
+                self.color_index = 2
+        else:
+            self.color_index = 0
     def drag_event(self, event):
         # 窗口移动的事件
         if event:
